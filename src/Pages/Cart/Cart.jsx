0e2fc52ce059bc66ac95590156c62adfdca1f537
mahisurrahman <zordanblog009@gmail.com>
2024-04-19 00:01:22 +0600

    Added the Increase Cart Stock

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import TitleAndSubtitle from "../../Components/TitleAndSubtitle/TitleAndSubtitle";
 import { FcDown, FcUp } from "react-icons/fc";
 
-
 const Cart = () => {
-const [cartItems, setCartItems] = useState()
+  const [cartItems, setCartItems] = useState();
 
-  let data = JSON.parse(localStorage.getItem("cart"));
-  console.log(data);
+  useEffect(() => {
+    const saveCart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(saveCart);
+  }, []);
 
-  const handleCartIncrease = (prod)=>{
-    // let increaseQty = prod.purchasedQuantity + 1;
+  const handleCartIncrease = (prod) => {
+    let prodId = prod.productId;
+    const updateCart = cartItems.map((item) => {
+      if (item.productId === prodId) {
+        const updatedQuantity = item.purchasedQuantity + 1;
+        const updatedTotalCost = updatedQuantity * item.sellingPrice;
+        return {
+          ...item,
+          purchasedQuantity: updatedQuantity,
+          totalPrice: updatedTotalCost,
+        };
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(updateCart));
+    setCartItems(updateCart);
   };
 
-  const handleCartDecrease = () =>{
-    console.log('Decrease');
+  const handleCartDecrease = () => {
+    // console.log('Decrease');
   };
 
-  const handleCartDelete = () =>{
-    console.log('Delete');
+  const handleCartDelete = () => {
+    // console.log('Delete');
   };
 
-  const handleCartConfirm = () =>{
-    console.log('Confirm');
-  }
+  const handleCartConfirm = () => {
+    // console.log('Confirm');
+  };
   return (
     <div>
       <Helmet>
@@ -49,7 +64,7 @@ const [cartItems, setCartItems] = useState()
                   <th>Confirm Product</th>
                 </tr>
               </thead>
-              {data.map((prod) => (
+              {cartItems?.map((prod) => (
                 <tbody
                   key={prod._id}
                   className="bg-transparent border-2 border-slate-900"
@@ -57,21 +72,39 @@ const [cartItems, setCartItems] = useState()
                   <tr className="">
                     <td className="font-bold">{prod.productName}</td>
                     <td>{prod.sellingPrice}</td>
-                    <td>
-                      {
-                        prod.stockQuantity - prod.purchasedQuantity
-                      }
-                    </td>
+                    <td>{prod.stockQuantity - prod.purchasedQuantity}</td>
                     <td>
                       <div className="flex items-center gap-8 w-8/12">
-                        <FcUp onClick={()=>{handleCartIncrease(prod)}} className="text-2xl duration-700 hover:duration-700 hover:scale-150 border-2 rounded-lg bg-slate-100"></FcUp>
+                        <FcUp
+                          onClick={() => {
+                            handleCartIncrease(prod);
+                          }}
+                          className="text-2xl duration-700 hover:duration-700 hover:scale-150 border-2 rounded-lg bg-slate-100"
+                        ></FcUp>
                         <p className="flex-grow">{prod.purchasedQuantity}</p>
-                        <FcDown onClick={handleCartDecrease} className="text-2xl duration-700 hover:duration-700 hover:scale-150 border-2 rounded-lg bg-slate-100"></FcDown>
+                        <FcDown
+                          onClick={handleCartDecrease}
+                          className="text-2xl duration-700 hover:duration-700 hover:scale-150 border-2 rounded-lg bg-slate-100"
+                        ></FcDown>
                       </div>
                     </td>
                     <td>{prod.totalPrice}$</td>
-                    <td><button onClick={handleCartDelete} className="px-6 py-2 bg-red-500 rounded-lg text-slate-100 font-bold text-md duration-700 hover:scale-125 hover:duration-700">Delete</button></td>
-                    <td><button onClick={handleCartConfirm} className="px-6 py-2 bg-green-500 rounded-lg text-slate-100 font-bold text-md duration-700 hover:scale-125 hover:duration-700">Confirm</button></td>
+                    <td>
+                      <button
+                        onClick={handleCartDelete}
+                        className="px-6 py-2 bg-red-500 rounded-lg text-slate-100 font-bold text-md duration-700 hover:scale-125 hover:duration-700"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={handleCartConfirm}
+                        className="px-6 py-2 bg-green-500 rounded-lg text-slate-100 font-bold text-md duration-700 hover:scale-125 hover:duration-700"
+                      >
+                        Confirm
+                      </button>
+                    </td>
                   </tr>
                 </tbody>
               ))}
